7ec9a00b461cfa9a418ce56897c5b3ee3d8e19e0
seydoux <seydoux@etud.insa-toulouse.fr>
2015-08-08 15:50:32 +0200

    Versionning of the UI files

---
+++ Programmation/workspace_cpp/roboinsat/robot/2015/Principal/librobot2015/DebugEnvironmentDisplay2015.cpp
@@ -0,0 +1,5 @@
+//
+// Created by seydoux on 26/04/15.
+//
+
+#include "DebugEnvironmentDisplay.h"
---
+++ Programmation/workspace_cpp/roboinsat/robot/2015/Principal/librobot2015/DebugEnvironmentDisplay2015.h
@@ -0,0 +1,17 @@
+//
+// Created by seydoux on 26/04/15.
+//
+
+#ifndef WORKSPACE_CPP_DEBUGENVIRONMENTDISPLAY_H
+#define WORKSPACE_CPP_DEBUGENVIRONMENTDISPLAY_H
+
+
+/**
+ * Cette classe vise à faire l'affichage de ce que le robot perçoit de la carte dans le simulateur.
+ */
+class DebugEnvironmentDisplay2015 {
+
+};
+
+
+#endif //WORKSPACE_CPP_DEBUGENVIRONMENTDISPLAY_H
